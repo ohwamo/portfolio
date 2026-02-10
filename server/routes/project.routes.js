9622587e67142e778355d8ca81ed4f4b17e3393d
ohwamo <147995742+ohwamo@users.noreply.github.com>
2026-02-10 17:23:40 +0400

    fix gitignore

--- server/routes/project.routes.js
+++
@@ -1,9 +0,0 @@
-import express from "express"
-import { getProject, createProject } from "../controller/project.controller.js"
-
-const router = express.Router()
-
-router.get("/", getProject)
-router.post("/", createProject)
-
-export default router;
